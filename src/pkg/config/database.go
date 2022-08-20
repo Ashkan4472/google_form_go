4a9ea2ef441d1f6a96bcf85ccd53289516c3b78e
Ashkan Tofangdar <ashkan4472@gmail.com>
2022-08-20 16:47:36 +0430

    added db export from database

--- src/pkg/config/database.go
+++ src/pkg/config/database.go
@@ -7,9 +7,12 @@ import (
 	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
 func InitialDatabase() {
+	var err error
 	dsn := os.Getenv("DATABASE_CONNECTION_STR")
-	_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic(err)
