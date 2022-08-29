50308224b1f18ee4680086c9723e12c7400950b8
Erfan Nikooee <nikooeeerfan@gmail.com>
2022-08-29 16:28:50 +0430

    added routes to main.go

--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	routesv1 "github.com/Ashkan4472/google_form_go/src/internals/routes/routes_v1"
 	"github.com/Ashkan4472/google_form_go/src/pkg/config"
 	"github.com/gofiber/fiber/v2"
 )
@@ -10,6 +11,11 @@ import (
 func initialize() {
 	config.InitialEnv()
 	config.InitialDatabase()
+}
+
+func setupRoutes(app *fiber.App) {
+	r := app.Group("api/v1")
+	routesv1.SetupAuthRoutes(r)
 }
 
 func main() {
@@ -20,6 +26,6 @@ func main() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
-
+	setupRoutes(app)
 	log.Fatal(app.Listen(":3000"))
 }
